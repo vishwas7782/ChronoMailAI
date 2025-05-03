00d2916d63ad6d7ef694db3444afac1c20c0c4a8
Vishwasjeet Kumar Gupta <vishwas7782@gmail.com>
2025-05-03 23:42:49 +0530

    React Updated

--- email-writer-react/src/App.jsx
+++ email-writer-react/src/App.jsx
@@ -30,7 +30,7 @@ function App() {
   return (
     <Container maxWidth="md" sx={{py:4}}>
       <Typography variant='h3' component="h1" gutterBottom>
-        Email Reply Generator
+      ChronoMail AI- Email Assistant
       </Typography>
 
       <Box sx={{ mx: 3 }}>
